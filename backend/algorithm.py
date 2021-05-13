af2d7d5885e7ba39b64140c3af58b98a6bf6552c
Marvin <mdtensuan_dev@protonmail.com>
2021-05-13 16:47:25 +0800

    Added main entry point to algorithm.py

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -10,6 +10,18 @@ def approx(f, a, b, c, d, size):
     xs = uniform(a, b, size)
     ys = uniform(c, d, size)
 
-    under = ys < eval_expr(f, {'xs: xs', numpy_dict})
+    area = (d-c) * (b-a)
+    under = ys < eval_expr(f, {'xs': xs}, numpy_dict)
 
     return sum(under) / size * area
+
+if __name__ == '__main__':
+    # python .\backend\algorithm.py 'sqrt(4 - xs**2)' 0 2 0 2
+    from sys import argv
+    f, a, b, c, d = argv[1:6]
+    size = int(argv[6]) if len(argv) == 7 else 100
+    a, b, c, d = map(int, (a, b, c, d))
+
+    print(f'Integrating {f} from {a} to {b} with {size} samples.')
+    result = approx(f, a, b, c, d, size)
+    print(f'{result = }')
